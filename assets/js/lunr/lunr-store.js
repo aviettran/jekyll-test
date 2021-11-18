c024093381e96c7a432645efa8681a506ee1c18e
Adam Tran <aviettran@gmail.com>
2021-11-17 20:34:17 -0500

    search

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -3,47 +3,68 @@ layout: none
 ---
 
 var store = [
-  {%- for c in site.collections -%}
-    {%- if forloop.last -%}
-      {%- assign l = true -%}
-    {%- endif -%}
-    {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
-    {%- for doc in docs -%}
-      {%- if doc.header.teaser -%}
-        {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
-      {%- else -%}
-        {%- assign teaser = site.teaser -%}
-      {%- endif -%}
-      {
-        "title": {{ doc.title | jsonify }},
-        "excerpt":
-          {%- if site.search_full_content == true -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | jsonify }},
-          {%- else -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-          {%- endif -%}
-        "categories": {{ doc.categories | jsonify }},
-        "tags": {{ doc.tags | jsonify }},
-        "url": {{ doc.url | relative_url | jsonify }},
-        "teaser": {{ teaser | relative_url | jsonify }}
-      }{%- unless forloop.last and l -%},{%- endunless -%}
-    {%- endfor -%}
-  {%- endfor -%}]
+{%- for family in site.data.families -%}
+    {
+      "title":
+    {{ "[Family] " | append: family.name | jsonify }},
+      "excerpt":
+    {{ family.description | newline_to_br |
+          replace:"<br />", " " |
+          replace:"</p>", " " |
+          replace:"</h1>", " " |
+          replace:"</h2>", " " |
+          replace:"</h3>", " " |
+          replace:"</h4>", " " |
+          replace:"</h5>", " " |
+          replace:"</h6>", " " |
+          replace:"\n", " " |
+        strip_html | strip_newlines | jsonify }},
+      "url":
+    {{ family.name | downcase | relative_url | jsonify }}
+    },
+{%- endfor -%}
+
+{%- for genus in site.data.genera -%}
+    {
+      "title":
+    {{ "[Genus] " | append: genus.name | jsonify }},
+      "excerpt":
+    {{ genus.description | newline_to_br |
+          replace:"<br />", " " |
+          replace:"</p>", " " |
+          replace:"</h1>", " " |
+          replace:"</h2>", " " |
+          replace:"</h3>", " " |
+          replace:"</h4>", " " |
+          replace:"</h5>", " " |
+          replace:"</h6>", " " |
+          replace:"\n", " " |
+        strip_html | strip_newlines | jsonify }},
+      "url":
+    {{ genus.family | append: "/" | append: genus.name | downcase | relative_url | jsonify }}
+    },
+{%- endfor -%}
+
+{%- for _species in site.data.species -%}
+    {
+      "title":
+    {{ "[Species] " | append: _species.genus | append: " " | append: _species.name | jsonify }},
+      "excerpt":
+    {{ _species.description | newline_to_br |
+          replace:"<br />", " " |
+          replace:"</p>", " " |
+          replace:"</h1>", " " |
+          replace:"</h2>", " " |
+          replace:"</h3>", " " |
+          replace:"</h4>", " " |
+          replace:"</h5>", " " |
+          replace:"</h6>", " " |
+          replace:"\n", " " |
+        strip_html | strip_newlines | jsonify }},
+      "url":
+    {{ _species.family | append: "/" | append: _species.genus | append: "/" | append: _species.name | downcase | relative_url | jsonify }}
+    }
+    {%- unless forloop.last -%},
+    {%- endunless -%}
+{%- endfor -%}
+]
