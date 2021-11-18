e7549fe0e55fc032a4446627d34614ac13d62a7f
aviettran <aviettran@users.noreply.github.com>
2021-11-18 01:35:09 +0000

    jekyll build from Action c024093381e96c7a432645efa8681a506ee1c18e

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,148 @@
+var store = [{
+      "title":
+    "[Family] Juglandaceae",
+      "excerpt":
+    "Walnut",
+      "url":
+    "/jekyll-test/juglandaceae"
+    },{
+      "title":
+    "[Family] Caprifoliaceae",
+      "excerpt":
+    "Honeysuckle",
+      "url":
+    "/jekyll-test/caprifoliaceae"
+    },{
+      "title":
+    "[Genus] Carya",
+      "excerpt":
+    "[contributed by Alan S. Weakley & Robert K. Peet] A genus of about 18 species, trees, of e. North America (south into s. Mexico), and e. Asia. Carya in our area is separated into two sections, section Apocarya (C. aquatica, C. cordiformis, C. illinoinensis) and section Carya (C. carolinae-septentrionalis, C. floridana, C. glabra, C. laciniosa, C. myristiciformis, C. ovalis, C. ovata, C. pallida, C. texana, C. tomentosa). The southeastern United States is the center of diversity of Carya. Our area includes all 13 North American species, and 13 of 18 species worldwide. Section Rhamphocarya includes a single Asian species. The remaining 4 species in the genus are all in section Apocarya: C. palmeri Manning of Mexico and 3 Asian species. C. cordiformis, C. aquatica, C. illinoinensis, C. myristiciformis, C. laciniosa, C. ovata, and C. carolinae-septentrionalis are diploids, with n = 16. C. pallida, C. floridana, C. texana, C. glabra, C. ovalis, and C. tomentosa are tetraploids with n = 32 (Stone 1961). As suggested by Stone, Adrouny, & Flake (1969), it seems possible that reticulate evolution involving extant or extinct diploid species is responsible for some of the difficulties in the C. glabra-ovalis complex. Many hybrids have been described, but some are questionable. Additionally, Hardin & Stone (1984) state that \"most of these hybrids are localized and have not led to introgressive populations, or at least none that have been recognized\". Ecologically, Carya is one of the more diverse and ubiquitous genera of trees in our area, surpassed in number of species, abundance, and ecological amplitude only by Quercus and Pinus. This has led to a long tradition of describing large parts of our area (in particular the Piedmont) as being characterized by \"oak-hickory\" or \"oak-pine-hickory\" forests (e.g. Küchler 1964; Greller 1988; Schafale & Weakley 1990; Skeen, Doerr, & Van Lear 1993). Ware (1992) and others have recently questioned this tradition, pointing out that Carya only rarely dominates or codominates, primarily in specialized circumstances (such as in soils with greater cation concentrations, derived from mafic rocks). The association of many (but certainly not all) species of hickories with soils with high base status was noted in print as early as 1820 in an account of the landscape of North Carolina. \"The sandy pine barrens, and all the lands on which pine is the exclusive growth, are unfriendly to agriculture; but where the pine is intermixed with oak and hickory, the soil is good. Some of our strongest lands have tall pine, mixed not only with hickory and oak, but also with walnut and cherry, and such trees that indicate the best soil. Where hickory prevails, the land is strong\" (Guthrie 1820). References: Elias (1972); Hardin & Stone (1984); Hardin (1952); Hardin (1992); Harrar & Harrar (1962); Little (1969); Manning (1950); Sargent (1918); Stone in Kubitzki, Rohwer, & Bittrich (1993); Stone in FNA3 (1997); Stone (1961); Stone, Adrouny, & Flake (1969). Identification Notes: Surface vestiture of leaves and bud scales is useful in distinguishing species of Carya. Some use of these characters can be made with a 10× or 20× hand lens; better still is a dissecting microscope. It is important to understand the different trichome types mentioned in the key (terminology follows Hardin 1990 and Hardin & Stone 1984). Short acicular trichomes are simple, unicellular trichomes tapered to a pointed tip, 0.10-0.35 mm long and with rough walls. Long acicular trichomes (\"solitary\" of Hardin & Stone 1984) are similar to short acicular, but are much larger, 0.45-1.6 mm long, and have smooth walls. Fasciculate trichomes are multicellular and have 2-8 straight or curled rays radiating from a clustered base. Multiradiate trichomes are similar to fasciculate, but have 8-17 rays, the inner (and usually more upright) rays attached basally above the outer (and usually more spreading) rays. Capitate glandular trichomes are unicellular or multicellular, and are distinguished by their bulbous or expanded tip; they are usually 0.02-0.1 mm long. Peltate scales are flat or dome-shaped shields or disks, slightly to strongly glandular, (sometimes regularly or irregularly lobed) and can be either sessile or stalked (they are often referred to as scales, resin dots, peltate glands, or lepidote scales). On the lower surfaces of leaflets, peltate scales are of two types: large peltate scales are 0.08-0.3 mm in diameter and are round, with smooth or slightly irregular margins, while small peltate scales are 0.025-0.12 mm in diameter and are either round, irregularly lobed or regularly 2- or 4-lobed.",
+      "url":
+    "/jekyll-test/juglandaceae/carya"
+    },{
+      "title":
+    "[Genus] Juglans",
+      "excerpt":
+    "Walnut",
+      "url":
+    "/jekyll-test/juglandaceae/juglans"
+    },{
+      "title":
+    "[Genus] Pterocarya",
+      "excerpt":
+    "Wingnut",
+      "url":
+    "/jekyll-test/juglandaceae/pterocarya"
+    },{
+      "title":
+    "[Genus] Viburnum",
+      "excerpt":
+    "Viburnum",
+      "url":
+    "/jekyll-test/caprifoliaceae/viburnum"
+    },{
+      "title":
+    "[Species] Carya aquatica",
+      "excerpt":
+    "description",
+      "url":
+    "/jekyll-test/juglandaceae/carya/aquatica"
+    },{
+      "title":
+    "[Species] Carya carolinae-septentrionalis",
+      "excerpt":
+    "description",
+      "url":
+    "/jekyll-test/juglandaceae/carya/carolinae-septentrionalis"
+    },{
+      "title":
+    "[Species] Carya cordiformis",
+      "excerpt":
+    "description",
+      "url":
+    "/jekyll-test/juglandaceae/carya/cordiformis"
+    },{
+      "title":
+    "[Species] Carya floridana",
+      "excerpt":
+    "description",
+      "url":
+    "/jekyll-test/juglandaceae/carya/floridana"
+    },{
+      "title":
+    "[Species] Carya glabra",
+      "excerpt":
+    "description",
+      "url":
+    "/jekyll-test/juglandaceae/carya/glabra"
+    },{
+      "title":
+    "[Species] Carya illinoinensis",
+      "excerpt":
+    "description",
+      "url":
+    "/jekyll-test/juglandaceae/carya/illinoinensis"
+    },{
+      "title":
+    "[Species] Carya laciniosa",
+      "excerpt":
+    "description",
+      "url":
+    "/jekyll-test/juglandaceae/carya/laciniosa"
+    },{
+      "title":
+    "[Species] Carya myristiciformis",
+      "excerpt":
+    "description",
+      "url":
+    "/jekyll-test/juglandaceae/carya/myristiciformis"
+    },{
+      "title":
+    "[Species] Carya ovalis",
+      "excerpt":
+    "description",
+      "url":
+    "/jekyll-test/juglandaceae/carya/ovalis"
+    },{
+      "title":
+    "[Species] Carya ovata",
+      "excerpt":
+    "description",
+      "url":
+    "/jekyll-test/juglandaceae/carya/ovata"
+    },{
+      "title":
+    "[Species] Carya pallida",
+      "excerpt":
+    "description",
+      "url":
+    "/jekyll-test/juglandaceae/carya/pallida"
+    },{
+      "title":
+    "[Species] Carya texana",
+      "excerpt":
+    "description",
+      "url":
+    "/jekyll-test/juglandaceae/carya/texana"
+    },{
+      "title":
+    "[Species] Carya tomentosa",
+      "excerpt":
+    "description",
+      "url":
+    "/jekyll-test/juglandaceae/carya/tomentosa"
+    },{
+      "title":
+    "[Species] Juglans nigra",
+      "excerpt":
+    "description",
+      "url":
+    "/jekyll-test/juglandaceae/juglans/nigra"
+    },{
+      "title":
+    "[Species] Juglans cinerea",
+      "excerpt":
+    "description",
+      "url":
+    "/jekyll-test/juglandaceae/juglans/cinerea"
+    }]
